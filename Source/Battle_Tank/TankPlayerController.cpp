46bd0a91a882fd2e83ed48cf389b51757916d4e3
The-Bobby-tarantinos <ethanbwalz@icloud.com>
2019-06-11 20:41:11 -0700

    commit #22

--- Source/Battle_Tank/TankPlayerController.cpp
+++ Source/Battle_Tank/TankPlayerController.cpp
@@ -38,7 +38,7 @@ void ATankPlayerController::AimTowardsCrosshair() {
 	FVector HitLocation; //Out parameter
 
 	if (GetSightRayHitLocation(HitLocation)) { // has side effect is going to line trace
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
 
 		//TODO tell controlled tank to aim at this point
 	}
@@ -47,8 +47,14 @@ void ATankPlayerController::AimTowardsCrosshair() {
 }
 
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const {
+	//find crosshair projection
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
 
-	HitLocation = FVector(1.0);
+
+	//"De-project" the screen position of the crosshair to a world direction
+	//line-trace along that look direction, and see what we hit (up to max range)
+	
 
 	return true;
 }
